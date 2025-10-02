9577c993cf9f9024d1e6ca8b972d7b7f68cb0dec
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-10-02 21:36:00 +0530

    Updated Files

--- client/src/components/reusableComponents/logo.tsx
+++ client/src/components/reusableComponents/logo.tsx
@@ -2,8 +2,18 @@
 
 import Image from "next/image";
 
-export default function Logo() {
+interface LogoProps {
+  width?: number; 
+}
+
+export default function Logo({ width = 100 }: LogoProps) {
   return (
-    <Image src={"/logo.png"} alt="Logo" width={100} height={100} className="w-20 py-2"/>
+    <Image
+      src="/logo.png"
+      alt="Logo"
+      width={100}
+      height={100} 
+      className={`py-2 w-${width}`}
+    />
   );
 }
